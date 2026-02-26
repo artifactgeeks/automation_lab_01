11fd6ec09c56edb69d5597e7a9ad4cd28a3b8bad
Kuldeep Chhipa <artifactgeeks@gmail.com>
2026-02-26 17:33:01 +0530

    added Lab003 program

---
+++ Automation_Lab/src/javaPackage/Lab003.java
@@ -0,0 +1,17 @@
+package javaPackage;
+
+public class Lab003 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("*********");	
+		System.out.println();
+		System.out.println("*********");
+		System.out.println("*********");
+		System.out.println("*********");
+		System.out.println("*********");
+		
+
+	}
+
+}
