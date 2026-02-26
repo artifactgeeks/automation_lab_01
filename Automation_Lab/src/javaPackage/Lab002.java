62829b153929bde2b65358021997019ac3a93100
Kuldeep Chhipa <artifactgeeks@gmail.com>
2026-02-26 17:07:05 +0530

    initial commit

---
+++ Automation_Lab/src/javaPackage/Lab002.java
@@ -0,0 +1,16 @@
+package javaPackage;
+
+public class Lab002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		/*vgfch
+		 * fgc
+		 * h
+		 * 
+		 */
+		System.out.println("Hello, Kuldeep this side and this is my First program in eclipse...");
+
+	}
+
+}
