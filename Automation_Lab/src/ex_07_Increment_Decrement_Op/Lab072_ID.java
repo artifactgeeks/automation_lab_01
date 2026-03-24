7f9b2856e7da0222331efba96222b7bf6f1e2732
Kuldeep Chhipa <artifactgeeks@gmail.com>
2026-03-24 20:18:49 +0530

    update the explaination

--- Automation_Lab/src/ex_07_Increment_Decrement_Op/Lab072_ID.java
+++ Automation_Lab/src/ex_07_Increment_Decrement_Op/Lab072_ID.java
@@ -4,7 +4,7 @@ public class Lab072_ID {
     public static void main(String[] args) {
         int a = 10;
         int result = ++a;
-        System.out.println(a);
+        System.out.println(a); 
         System.out.println(result);
 
 
--- Automation_Lab/src/ex_07_Increment_Decrement_Op/Lab074_Advance_ID.java
+++ Automation_Lab/src/ex_07_Increment_Decrement_Op/Lab074_Advance_ID.java
@@ -4,6 +4,7 @@ public class Lab074_Advance_ID {
     public static void main(String[] args) {
         int a = 10;
         System.out.println(a++ + a);
+     
         // A + B
         // A -> a++ -> ExpA -> 10 , a =11
         // +
