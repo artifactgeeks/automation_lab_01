dfac44f3877816be7d71d2055afde70a47dbcbf7
Kuldeep Chhipa <artifactgeeks@gmail.com>
2026-03-26 22:03:07 +0530

    Add voting eligibility and switch case examples in new Java classes

---
+++ Automation_Lab/src/ex_09_Switch/Lab085_Switch_Without_Break.java
@@ -0,0 +1,37 @@
+package ex_09_Switch;
+
+import java.util.Scanner;
+
+public class Lab085_Switch_Without_Break {
+      // Switch
+
+        // Take a user input and tell them the day which they have told.
+        // 1 to 7 -> 1 -> mon, 5 -> fri
+        // 8 ? - Not allowed or error
+
+        public static void main(String[] args) {
+        //int day = Integer.parseInt(args[0]); - CLI option
+          Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the day num(1 to 7)");
+        int day = scanner.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+            case 2:
+                System.out.println("Tuesday");
+            case 3:
+                System.out.println("Wednesday");
+            case 4:
+                System.out.println("Thursday");
+            case 5:
+                System.out.println("Friday");
+            case 6:
+                System.out.println("Saturday");
+            case 7:
+                System.out.println("Sunday");
+            default:
+                System.out.println("Not allowed or error");
+        }
+        }
+
+}
